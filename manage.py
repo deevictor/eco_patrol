18744d4efa4c32a19955899bfab3867fb2ba1cb7
Viktor Di <viktor.di@fogstream.ru>
2018-11-09 13:58:14 +1000

    initial commit

--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ if __name__ == '__main__':
 
     sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
     sys.path.append(os.path.join(os.path.dirname(__file__), '.'))
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ecomap.settings_local')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'eco_patrol.settings_local')
 
     from django.core.management import execute_from_command_line
 
